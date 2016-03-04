857844d3e734af7925b8442d4ae8f315a4a7f2ce
Mostafa Samir <mostafa.3210@gmail.com>
2016-03-04 02:01:03 +0200

    fixed errors in test case and fixture

--- tests/fixtures/beacon/fake-tcp-server.js
+++ tests/fixtures/beacon/fake-tcp-server.js
@@ -1,5 +1,12 @@
 var ipc = require('node-ipc');
 
-ipc.config.silent = true;
+ipc.config.silent = false;
 
-ipc.serveNet('127.0.0.1', 4895, function() {});
+ipc.serveNet('127.0.0.1', 4895, function() {
+
+    ipc.server.on('PROBE:MSG', function(data, socket) {
+        ipc.server.emit(socket, "ALIVE:MSG", {});
+    });
+});
+
+ipc.server.start();
--- tests/specs/beacon/beacon-remote.specs.js
+++ tests/specs/beacon/beacon-remote.specs.js
@@ -4,22 +4,21 @@ var spawn = require('child_process').spawn;
 
 describe("Beacon Remote Communincation", function() {
 
+    this.timeout(5000);
+
     it('connects/disconnects to/from a running tcp server', function(done) {
 
         var fake_server = spawn('node', ['./tests/fixtures/beacon/fake-tcp-server.js']);
-        var fake_server_stdout = "";
-        fake_server.stdout.on('data', function(chunck){ fake_server_stdout += chunck.toString().trim(); });
 
         tcp.connectTo('127.0.0.1', 4895)
         .then(function(connection) {
             expect(!!connection).to.equal(true);
-
+            expect(connection.socket.destroyed).to.equal(false);
             tcp.disconnectFrom('127.0.0.1', 4895);
-            expect(!!connection).to.equal(false);
-
-            fake_server.kill();
+            expect(connection.socket.destroyed).to.equal(true);
             done();
-        });
+        })
+        .catch(done);
     });
 
 });
