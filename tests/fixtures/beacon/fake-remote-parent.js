b03804623b83d325f02a1f8fab506f868eb3927e
Mostafa Samir <mostafa.3210@gmail.com>
2016-03-04 23:52:59 +0200

    fixed semantic error in fixture

--- tests/fixtures/beacon/fake-remote-parent.js
+++ tests/fixtures/beacon/fake-remote-parent.js
@@ -3,8 +3,8 @@ var ipc = require('node-ipc');
 ipc.config.silent = true;
 
 ipc.serveNet('127.0.0.1', 4895, function() {
-    ipc.server.on('MONITOR:MSG', function(data, socket) {
-        console.log(data.line);
+    ipc.server.on('MONITOR:MSG', function(msg, socket) {
+        console.log(msg.data.line);
         socket.destroy();
         ipc.server.stop();
     });
