66e39d251fd610f12fd140ce219ae7fd1d43fbd3
Mostafa Samir <mostafa.3210@gmail.com>
2016-03-31 00:49:18 +0200

    added tests for invalid hosts parsing

--- tests/specs/cli/controller.specs.js
+++ tests/specs/cli/controller.specs.js
@@ -15,5 +15,55 @@ describe('CLI Controller', function() {
         expect(hosts["192.168.0.103@2222:h3llo"]).to.equal(5);
         expect(hosts["192.168.0.20@5986:oll3h"]).to.equal(1);
 
-    })
-})
+    });
+
+    it('fails to parse a non-existing hosts file', function() {
+        var hosts = cli.parseHosts("fake-hosts.that.is.not.there.json");
+
+        expect(hosts.error).to.exist;
+        expect(hosts.error).to.have.string('ENOENT');
+    });
+
+    it('fails to parse a non-json hosts file', function() {
+        var hosts = cli.parseHosts(__dirname + "/cli.specs.js");
+
+        expect(hosts.error).to.exist;
+        expect(hosts.error).to.have.string('Unexpected token');
+    });
+
+    it('fails to parse a hosts file with non-object entry', function() {
+        var hosts = cli.parseHosts(__dirname + "/../../fixtures/cli/fake-hosts.invalid-1.json");
+
+        expect(hosts.error).to.exist;
+        expect(hosts.error).to.have.string('INVALID: an entry in hosts file is not an object');
+    });
+
+    it('fails to parse a hosts file with an entry of a non-number pCount', function() {
+        var hosts = cli.parseHosts(__dirname + "/../../fixtures/cli/fake-hosts.invalid-2.json");
+
+        expect(hosts.error).to.exist;
+        expect(hosts.error).to.have.string("INVALID: an entry's processes count is not a number");
+    });
+
+    it('fails to parse a hosts file with an entry of a non-positive pCount', function() {
+        var hosts = cli.parseHosts(__dirname + "/../../fixtures/cli/fake-hosts.invalid-5.json");
+
+        expect(hosts.error).to.exist;
+        expect(hosts.error).to.have.string("INVALID: an entry's processes count is less than one");
+    });
+
+    it('fails to parse a hosts file with an entry of a non-number port', function() {
+        var hosts = cli.parseHosts(__dirname + "/../../fixtures/cli/fake-hosts.invalid-3.json");
+
+        expect(hosts.error).to.exist;
+        expect(hosts.error).to.have.string("INVALID: an entry's port number is not a number!");
+    });
+
+    it('fails to parse a hosts file with an entry of a non-string password', function() {
+        var hosts = cli.parseHosts(__dirname + "/../../fixtures/cli/fake-hosts.invalid-4.json");
+
+        expect(hosts.error).to.exist;
+        expect(hosts.error).to.have.string("INVALID: an entry's password is not a string");
+    });
+
+});
