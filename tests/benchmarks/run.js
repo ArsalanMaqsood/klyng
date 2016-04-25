b069bdb650cbf22b859defcdf0ec9b9f5c804444
Mostafa Samir <mostafa.3210@gmail.com>
2016-04-25 14:17:44 +0200

    updated benchmarks layout

--- tests/benchmarks/run.js
+++ tests/benchmarks/run.js
@@ -25,29 +25,29 @@ function run(task) {
     console.log("");
 
     var start = new Promise(function(resolve, reject) {
-        console.log("  Running Sequentially".yellow);
+        console.log("    Running Sequentially".yellow);
         var regular_path = './tests/benchmarks/tasks/' + task.alias + '/regular.js';
         spawn_node([regular_path], function(time) {
-            console.log("    " + time.italic);
+            console.log("      " + time.italic);
             resolve();
         });
     })
     .then(function () {
         return new Promise(function(resolve, reject) {
-            console.log("  Running with klyng -n 2".yellow);
+            console.log("    Running with klyng -n 2".yellow);
             var klyng_path = './tests/benchmarks/tasks/' + task.alias + '/klyng.js';
             spawn_node(['./bin/main.js', '-n', '2', klyng_path], function(time) {
-                console.log("    " + time.italic);
+                console.log("      " + time.italic);
                 resolve();
             });
         });
     })
     .then(function() {
         return new Promise(function(resolve, reject) {
-            console.log("  Running with klyng -n 4".yellow);
+            console.log("    Running with klyng -n 4".yellow);
             var klyng_path = './tests/benchmarks/tasks/' + task.alias + '/klyng.js';
             spawn_node(['./bin/main.js', '-n', '4', klyng_path], function(time) {
-                console.log("    " + time.italic);
+                console.log("      " + time.italic);
                 resolve();
             });
         });
@@ -75,7 +75,7 @@ var tasks = [
     {
         name: "Pi Approximation",
         alias: "pi",
-        description: "Approximates pi with using arctan integral formula"
+        description: "Approximates pi using arctan integral formula (dx = 5e-9)"
     }
 ];
 
